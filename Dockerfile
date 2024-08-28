72d7e33e0c43a62f0f50fd0a0b39cad263f05b0a
kerimovok <naxro.ok1@gmail.com>
2024-08-28 22:27:51 +0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM oven/bun:1 as base
+WORKDIR /usr/src/app
+
+FROM base AS install
+RUN mkdir -p /temp/dev
+COPY package.json bun.lockb /temp/dev/
+RUN cd /temp/dev && bun install --frozen-lockfile
+
+RUN mkdir -p /temp/prod
+COPY package.json bun.lockb /temp/prod/
+RUN cd /temp/prod && bun install --frozen-lockfile --production
+
+FROM base AS prerelease
+COPY --from=install /temp/dev/node_modules node_modules
+COPY . .
+
+ENV NODE_ENV=production
+
+FROM base AS release
+COPY --from=install /temp/prod/node_modules node_modules
+COPY --from=prerelease /usr/src/app/index.ts .
+COPY --from=prerelease /usr/src/app/package.json .
+
+RUN apt-get update && apt-get install gnupg wget -y && \
+    wget -q -O- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg && \
+    sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
+    apt-get update && \
+    apt-get install google-chrome-stable -y --no-install-recommends && \
+    rm -rf /var/lib/apt/lists/*
+
+ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+
+USER root
+RUN chown -R bun:bun /usr/src/app/node_modules
+USER bun
+ENTRYPOINT [ "bun", "run", "index.ts" ]
---
+++ index.ts
@@ -0,0 +1,90 @@
+import puppeteer from "puppeteer";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = process.env.API_TOKEN || 3001;
+app.use(bodyParser.json());
+app.use(cors());
+
+const basePuppeteerOptions = {
+  headless: true,
+  executablePath:
+    process.env.NODE_ENV === "production"
+      ? process.env.PUPPETEER_EXECUTABLE_PATH
+      : puppeteer.executablePath(),
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+};
+
+app.post("/screenshot", async (req, res) => {
+  const { url, viewportWidth, viewportHeight, userAgent, extraHTTPHeaders } =
+    req.body;
+  let browser;
+
+  try {
+    browser = await puppeteer.launch(basePuppeteerOptions);
+    const page = await browser.newPage();
+
+    if (viewportWidth && viewportHeight) {
+      await page.setViewport({ width: viewportWidth, height: viewportHeight });
+    }
+
+    if (userAgent) {
+      await page.setUserAgent(userAgent);
+    }
+
+    if (extraHTTPHeaders) {
+      await page.setExtraHTTPHeaders(extraHTTPHeaders);
+    }
+
+    await page.goto(url, { waitUntil: "networkidle2" });
+
+    const screenshotBuffer = await page.screenshot({ encoding: "base64" });
+    res.send({ image: screenshotBuffer });
+  } catch (error) {
+    console.error("Error taking screenshot:", error);
+    res.status(500).send({ error: "Failed to take screenshot" });
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
+});
+
+app.post("/metadata", async (req, res) => {
+  let browser;
+
+  try {
+    browser = await puppeteer.launch(basePuppeteerOptions);
+    const page = await browser.newPage();
+    await page.goto(req.body.url, { waitUntil: "networkidle2" });
+
+    const metadata = await page.evaluate(() => {
+      const getMetaContent = (name: string) =>
+        document
+          .querySelector(`meta[property='og:${name}']`)
+          ?.getAttribute("content") ||
+        document.querySelector(`meta[name='${name}']`)?.getAttribute("content");
+
+      return {
+        ogTitle: getMetaContent("title") || document.title,
+        ogDescription: getMetaContent("description"),
+        ogImage: getMetaContent("image"),
+      };
+    });
+
+    res.send(metadata);
+  } catch (error) {
+    console.error("Error extracting metadata:", error);
+    res.status(500).send({ error: "Failed to extract metadata" });
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
+});
