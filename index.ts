a9acc9e2a35ebef6674462bc4af6fac60f57d15b
kerimovok <naxro.ok1@gmail.com>
2024-08-28 23:00:27 +0400

    fixed: Env variable name for port

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
-const port = process.env.API_TOKEN || 3001;
+const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(cors());
 
